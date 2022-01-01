38f0e6a3f9a7a159201b192e46b79231c20f65ae
jstafford <CatMouse2017!>
2022-01-01 14:21:56 -0600

    Added the rest of the unit tests from codewars. All passed.

--- tests/ArrayDiff.test.js
+++ tests/ArrayDiff.test.js
@@ -12,4 +12,16 @@ describe("For ArrayDiff,", () => {
     test(`Verify that ArrayDiff returns "[1, 3]" if provided "[1, 2, 2, 2, 2, 3]" for first array and "[2]" for second array.`, () => {
         expect(ArrayDiff([1, 2, 2, 2, 2, 3], [2])).toStrictEqual([1, 3]);
     });
+
+    test(`Verify that ArrayDiff returns "[]" if provided "[]" for first array and "[4, 5]" for second array.`, () => {
+        expect(ArrayDiff([], [4, 5])).toStrictEqual([]);
+    });
+
+    test(`Verify that ArrayDiff returns "[1, 8, 2]" if provided "[1, 8, 2]" for first array and "[]" for second array.`, () => {
+        expect(ArrayDiff([1, 8, 2], [])).toStrictEqual([1, 8, 2]);
+    });
+
+    test(`Verify that ArrayDiff returns "[1, 2, 3]" if provided "[1, 2]" for first array and "[3]" for second array.`, () => {
+        expect(ArrayDiff([1, 2, 3], [1, 2])).toStrictEqual([3]);
+    });
 });
